d26f8e888eb0595cd6528508fecb98a98025d826
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-03-19 15:32:47 +0100

    Modify calculate distance test - add situations when k-point is not passed

--- calculateDistancePoints.test.js
+++ calculateDistancePoints.test.js
@@ -3,24 +3,45 @@ const calculateDistancePoints = require('./calculateDistancePoints');
 
 describe('calculateDistancePoints', () => {
 
-    // normal hill
+    // normal hill - passed k-Point
     it('should return return 86 points', () => {
         const actual = calculateDistancePoints(111, 109, 98);
         const expected = '86';
         assert.equal(actual, expected);
     });
 
-    // large hill
+    // large hill - passed k-Point
     it('should return return 85.2 points', () => {
         const actual = calculateDistancePoints(134.0, 134, 120);
         const expected = '85.2';
         assert.equal(actual, expected);
     });
 
-    // mammoth hill
+    // mammoth hill - passed k-Point
     it('should return return 153 points', () => {
         const actual = calculateDistancePoints(227.5, 225, 200);
         const expected = '153';
+        assert.equal(actual, expected);
+    });
+
+    // normal hill - not passed k-Point
+    it('should return return 40 points', () => {
+        const actual = calculateDistancePoints(88, 109, 98);
+        const expected = '40';
+        assert.equal(actual, expected);
+    });
+
+    // large hill - not passed k-Point
+    it('should return return 47.4 points', () => {
+        const actual = calculateDistancePoints(113, 134, 120);
+        const expected = '47.4';
+        assert.equal(actual, expected);
+    });
+
+    // mammoth hill - not passed k-Point
+    it('should return return 112.8 points', () => {
+        const actual = calculateDistancePoints(194, 225, 200);
+        const expected = '112.8';
         assert.equal(actual, expected);
     });
 
